56b0559c92217254ff2880f1df2805c91b6c94e8
José Miguel Moreno <josemmo@pm.me>
2022-06-15 11:00:08 +0200

    Added support for 1.19
    - Updated SpawnEntityPacket class
    - Upgraded dependencies
    - Updated README.md

--- src/main/java/io/josemmo/bukkit/plugin/packets/SpawnEntityPacket.java
+++ src/main/java/io/josemmo/bukkit/plugin/packets/SpawnEntityPacket.java
@@ -2,11 +2,25 @@ package io.josemmo.bukkit.plugin.packets;
 
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketContainer;
+import io.josemmo.bukkit.plugin.utils.Internals;
 import org.bukkit.entity.EntityType;
 import org.jetbrains.annotations.NotNull;
 import java.util.UUID;
 
 public class SpawnEntityPacket extends PacketContainer {
+    private static final boolean ROTATION_AS_BYTES;
+    private static final int DATA_INDEX;
+
+    static {
+        if (Internals.MINECRAFT_VERSION < 19) {
+            ROTATION_AS_BYTES = false;
+            DATA_INDEX = 6;
+        } else {
+            DATA_INDEX = 4;
+            ROTATION_AS_BYTES = true;
+        }
+    }
+
     public SpawnEntityPacket() {
         super(PacketType.Play.Server.SPAWN_ENTITY);
         getIntegers()
@@ -28,14 +42,20 @@ public class SpawnEntityPacket extends PacketContainer {
     }
 
     public @NotNull SpawnEntityPacket setRotation(int pitch, int yaw) {
-        getIntegers()
-            .write(4, pitch)
-            .write(5, yaw);
+        if (ROTATION_AS_BYTES) {
+            getBytes()
+                .write(0, (byte) pitch)
+                .write(1, (byte) yaw);
+        } else {
+            getIntegers()
+                .write(4, pitch)
+                .write(5, yaw);
+        }
         return this;
     }
 
     public @NotNull SpawnEntityPacket setData(int data) {
-        getIntegers().write(6, data);
+        getIntegers().write(DATA_INDEX, data);
         return this;
     }
 
