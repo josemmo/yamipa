de11efac71ed871681502ad04d1de32264e5e342
José Miguel Moreno <josemmo@pm.me>
2025-07-07 20:57:46 +0200

    Added support for Minecraft 1.21.7
    - Updated EntityMetadataPacket class
    - Updated README.md
    
    > Closes #149
    
    Co-Authored-By: まっする <158264076+musclepr@users.noreply.github.com>

--- src/main/java/io/josemmo/bukkit/plugin/packets/EntityMetadataPacket.java
+++ src/main/java/io/josemmo/bukkit/plugin/packets/EntityMetadataPacket.java
@@ -21,7 +21,13 @@ public class EntityMetadataPacket extends PacketContainer {
 
     static {
         USE_DATA_WATCHER = (Internals.MINECRAFT_VERSION < 19.3);
-        ITEM_INDEX = (Internals.MINECRAFT_VERSION < 17) ? 7 : 8;
+        if (Internals.MINECRAFT_VERSION < 17) { // Minecraft 1.16.x
+            ITEM_INDEX = 7;
+        } else if (Internals.MINECRAFT_VERSION < 21.6) { // Minecraft 1.17.x to 1.21.5
+            ITEM_INDEX = 8;
+        } else { // Minecraft 1.21.6 and upwards
+            ITEM_INDEX = 9;
+        }
         ROTATION_INDEX = ITEM_INDEX + 1;
     }
 
